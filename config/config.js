8af24ae6d5e4e28429f131276e0efccc10a786da
Mikkke <jkellymichael@gmail.com>
2020-09-02 08:48:00 +0200

    yes its commit

--- config/config.js
+++ config/config.js
@@ -9,10 +9,10 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE_URL,
-    host: process.env.DB_HOST,
-    dialect: "postgres"
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: true
+    }
   }
 };
